ddd91773f73d4bf578c321a41e70bd1b8cb623cb
natsuki-kining <natsuki_kining@126.com>
2020-07-28 14:32:23 +0800

    emmmmm……最近没空

--- file/sql-file/Oracle/table.sql
+++ file/sql-file/Oracle/table.sql
@@ -1,61 +1,61 @@
 -- Create table
 create table SSR_DYNAMIC_SQL
 (
-  id            VARCHAR2(32) not null,
-  query_code    VARCHAR2(100) not null,
-  query_name    VARCHAR2(100),
-  query_type    VARCHAR2(50),
-  sql_template  CLOB not null,
-  before_script CLOB,
-  after_script  CLOB,
-  version       VARCHAR2(20),
-  del_flag      NUMBER(1),
-  order_num     NUMBER(11),
-  create_name   VARCHAR2(60),
-  create_id     VARCHAR2(64),
-  create_time   TIMESTAMP(6),
-  update_name   VARCHAR2(60),
-  update_id     VARCHAR2(64),
-  update_time   TIMESTAMP(6),
-  remark        VARCHAR2(1000)
+  ID            VARCHAR2(32) not null,
+  QUERY_CODE    VARCHAR2(100) not null,
+  QUERY_NAME    VARCHAR2(100),
+  QUERY_TYPE    VARCHAR2(50),
+  SQL_TEMPLATE  CLOB not null,
+  BEFORE_SCRIPT CLOB,
+  AFTER_SCRIPT  CLOB,
+  VERSION       VARCHAR2(20),
+  DEL_FLAG      NUMBER(1),
+  ORDER_NUM     NUMBER(11),
+  CREATE_NAME   VARCHAR2(60),
+  CREATE_ID     VARCHAR2(64),
+  CREATE_TIME   TIMESTAMP(6),
+  UPDATE_NAME   VARCHAR2(60),
+  UPDATE_ID     VARCHAR2(64),
+  UPDATE_TIME   TIMESTAMP(6),
+  REMARK        VARCHAR2(1000)
 );
 -- Add comments to the table 
 comment on table SSR_DYNAMIC_SQL
   is '动态SQL';
 -- Add comments to the columns 
-comment on column SSR_DYNAMIC_SQL.id
+comment on column SSR_DYNAMIC_SQL.ID
   is '主键';
-comment on column SSR_DYNAMIC_SQL.query_code
+comment on column SSR_DYNAMIC_SQL.QUERY_CODE
   is '查询的code';
-comment on column SSR_DYNAMIC_SQL.query_name
+comment on column SSR_DYNAMIC_SQL.QUERY_NAME
   is '查询的名称';
-comment on column SSR_DYNAMIC_SQL.query_type
+comment on column SSR_DYNAMIC_SQL.QUERY_TYPE
   is '查询的类型';
-comment on column SSR_DYNAMIC_SQL.sql_template
+comment on column SSR_DYNAMIC_SQL.SQL_TEMPLATE
   is 'sql模板';
-comment on column SSR_DYNAMIC_SQL.before_script
+comment on column SSR_DYNAMIC_SQL.BEFORE_SCRIPT
   is '查询之前的处理脚本。';
-comment on column SSR_DYNAMIC_SQL.after_script
+comment on column SSR_DYNAMIC_SQL.AFTER_SCRIPT
   is '查询之后的处理脚本。';
-comment on column SSR_DYNAMIC_SQL.version
+comment on column SSR_DYNAMIC_SQL.VERSION
   is '版本号';
-comment on column SSR_DYNAMIC_SQL.del_flag
+comment on column SSR_DYNAMIC_SQL.DEL_FLAG
   is '是否删除。1：是，0：否。';
-comment on column SSR_DYNAMIC_SQL.order_num
+comment on column SSR_DYNAMIC_SQL.ORDER_NUM
   is '排序编号。正序。';
-comment on column SSR_DYNAMIC_SQL.create_name
+comment on column SSR_DYNAMIC_SQL.CREATE_NAME
   is '创建人名称';
-comment on column SSR_DYNAMIC_SQL.create_id
+comment on column SSR_DYNAMIC_SQL.CREATE_ID
   is '创建人id';
-comment on column SSR_DYNAMIC_SQL.create_time
+comment on column SSR_DYNAMIC_SQL.CREATE_TIME
   is '创建时间';
-comment on column SSR_DYNAMIC_SQL.update_name
+comment on column SSR_DYNAMIC_SQL.UPDATE_NAME
   is '修改人名称';
-comment on column SSR_DYNAMIC_SQL.update_id
+comment on column SSR_DYNAMIC_SQL.UPDATE_ID
   is '修改人id';
-comment on column SSR_DYNAMIC_SQL.update_time
+comment on column SSR_DYNAMIC_SQL.UPDATE_TIME
   is '修改时间';
-comment on column SSR_DYNAMIC_SQL.remark
+comment on column SSR_DYNAMIC_SQL.REMARK
   is '备注';
 -- Create/Recreate primary, unique and foreign key constraints 
 alter table SSR_DYNAMIC_SQL
