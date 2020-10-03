15ac3140f32d62f87c7f547a0d4696b70fd77a76
natsuki-kining <natsuki_kining@126.com>
2020-10-03 12:04:07 +0800

    多数据源警告提示

--- ssr-core/src/main/java/com/natsuki_kining/ssr/core/config/multisource/DynamicDataSource.java
+++ ssr-core/src/main/java/com/natsuki_kining/ssr/core/config/multisource/DynamicDataSource.java
@@ -2,6 +2,8 @@ package com.natsuki_kining.ssr.core.config.multisource;
 
 import com.natsuki_kining.ssr.core.utils.Constant;
 import com.natsuki_kining.ssr.core.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
@@ -10,13 +12,17 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * @author xiongneng
  * @since 2017年3月5日 上午9:11:49
  */
+@Slf4j
 public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    @Autowired
+    private MultiSourceConfig multiSourceConfig;
 
     @Override
     protected Object determineCurrentLookupKey() {
         String dataSourceName = DataSourceContextHolder.getDataSourceName();
-        if (StringUtils.isBlank(dataSourceName)){
-            dataSourceName = Constant.MultiDataSource.masterDataSourceName;
+        if (StringUtils.isNotBlank(dataSourceName) && !multiSourceConfig.multiDruidDataSourceMap.containsKey(dataSourceName)){
+            log.warn("没有找到数据源：{}，将使用默认数据源：{}。",dataSourceName,Constant.MultiDataSource.masterDataSourceName);
         }
         return dataSourceName;
     }
